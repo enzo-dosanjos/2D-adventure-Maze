3f82ef1faefc015d2fdc1d72dc9d3cd1c7d98ed1
Emma <119598417+ritafari@users.noreply.github.com>
2024-05-31 17:52:34 +0200

    main

---
+++ GameFiles/Main.py
@@ -0,0 +1,26 @@
+from GameFiles.MazeGame import MazeGame
+from GameFiles.MazeGameGUI import MazeGUI
+from GameFiles.Monster import Monster
+from GameFiles.Player import Player
+from GameFiles.GameElements import Treasure
+
+def main():
+    maze = MazeGame((55, 55))  # maze has to be at least 9 in height and length because the maze is surrounded by walls and needs to generate at least a path inside
+    maze.generate_maze()
+    # maze.print_maze()
+
+    player = Player(maze.game_state)
+
+    monster = Monster(maze.game_state)
+
+    Treasure(maze.game_state)
+
+    Gui = MazeGUI(maze.game_state, monster)
+
+    #print(maze.game_state)
+
+
+
+
+if __name__ == "__main__":
+    main()
